24d0fc928d39c7779e8edd387ac4a4071fafb5b5
Nicole Kelly <nrkelly57@gmail.com>
2023-11-22 19:40:45 +1300

    commit final update for practice

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Server is up and running, new image!')
+  res.send('Update to image, server is up and running!')
 })
 
 app.listen(port, () => {
