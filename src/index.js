c17d29c7c5d5ddd88802016ef5e7a2dd16e67c53
Nicole Kelly <nrkelly57@gmail.com>
2023-11-29 19:18:48 +1300

    updated response message

---
+++ src/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const app = require('express')();
+const port = process.env.port || 8080;
+const env = process.env.NODE_ENV;
+
+app.get('/', (req, res) => {
+  res.send(`Server is up and running. This is a class demo in ${env}.`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
